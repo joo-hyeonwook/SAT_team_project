e5b721e326d56b17865e13b1dfb86b531c88faa9
pizzaman <pizzaman3849@gmail.com>
2025-11-28 20:36:52 +0900

    chore: 헤더파일 수정
    Enemy 클래스의 전방선언과 Enemy, Player 클래스의 생성자 명시적 선언을 추가하고 각 함수 선언에 달린 주석의 가독성을 높이기 위해 주석이 함수 선언 아랫줄에 개행되어 작성된 경우에 주석 밑에 빈 줄을 추가함.

--- include/rpg.h
+++ include/rpg.h
@@ -22,19 +22,29 @@ class Character {
     void setHp(int atk = 10, int def = 100);//hp를newHp로 수정 
 
 };
+
+//전방 선언
+class Enemy;
+
 class Player : public Character {
     public:
+    Player();
     void move();
     int getPlayerAction(); //플레이어 행동 선택 후 리턴
-    void playerAttack(Enemy& refEnemy, int enemyDef); //주사위 값에 따른 피해량만큼 적 체력 차감
+    void playerAttack(Enemy& refEnemy, int enemyDef); 
+    //주사위 값에 따른 피해량만큼 적 체력 차감
+
     void playerDefend();//주사위 값에 따른 방어력만큼 플레이어가 받는 피해 경감
     bool run();//주사위 값에 따른 성공여부에 따라 전투 종료 혹은 행동 무효화
 };
 
 class Enemy : public Character {
     public:
+    Enemy();
     int getEnemyAction(); //적 행동 무작위 결정 후 리턴
-    void enemyAttack(Player& refPlayer, int playerDef);//주사위 값에 따른 피해량만큼 플레이어 체력 차감
+    void enemyAttack(Player& refPlayer, int playerDef);
+    //주사위 값에 따른 피해량만큼 플레이어 체력 차감
+    
     void enemyDefend();//주사위 값에 따른 방어력만큼 플레이어가 받는 피해 경감
 };
 
@@ -42,12 +52,15 @@ namespace GameManager {
     void printTutorial();//튜토리얼 출력
     void printMap(Player& refPlayer, Enemy& refEnemyA, Enemy& refEnemyB, Enemy& refEnemyC);
     //맵과 플레이어, 출구 위치(P, E) 출력 
+
     int getDice();//주사위 함수, 2~12 사이의 무작위 정수 반환
     void ifBattleStart(Player& refPlayer, Enemy& refEnemyA, Enemy& refEnemyB, Enemy& refEnemyC);
     // 플레이어와 적 객체의 위치 비교 후 플레이어와 적의 위치가 겹칠 시 전투 함수 호출
     //전투 함수는 플레이어와 위치가 겹치는 적을 참조 매개 변수로 함
+
     void battle(Player &refPlayer, Enemy &refEnemy);
     //둘 중 하나의 체력이 0이 될 때까지 플레이어와 적의 행동을 번갈아 선택 후 실행 
+
     void gameOver();//게임오버 메시지 출력 후 파일 종료
     void ifGameClear(Player &refPlayer);//플레이어의 위치에 따라 게임 클리어 여부를 판단
     void gameClear();//게임 클리어 메시지 출력 후 파일 종료

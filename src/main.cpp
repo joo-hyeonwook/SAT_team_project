89ccec121ab21abb5e73802726df4e3d92bb6c28
pizzaman <pizzaman3849@gmail.com>
2025-12-01 00:26:05 +0900

    chore: main 함수 수정
    게임플레이 기획에 맞게 돌아가게끔 main 함수 내의 로직을 수정함.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <windows.h>
 #include "../include/rpg.h"
 
 using namespace GameManager;
 
 int main() {
+    //유니코드로 출력 설정(안하면 한글 깨짐)
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     Player player;
     Enemy enemy[3]; 
+    enemyLocationInit(enemy[0], enemy[1], enemy[2]);
+    
+    printTutorial();
     while(1) {
         printMap(player, enemy[0], enemy[1], enemy[2]);
         while(1) {
             player.move();
-            ifBattleStart(player, enemy[0], enemy[1], enemy[2]);
+            ifGameClear(player);
+            //ifBattleStart(player, enemy[0], enemy[1], enemy[2]);
+            printMap(player, enemy[0], enemy[1], enemy[2]);
         }
-        ifGameClear(player);
     }
 
     return 0;
